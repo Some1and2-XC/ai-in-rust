b3dff2e8a9b57b967eb302fcdad3c9048d742178
Mark <04x0xx@gmail.com>
2024-10-07 14:22:23 -0300

    Implemented generics and fixed links between layers

--- src/library/node.rs
+++ src/library/node.rs
@@ -1,11 +1,47 @@
-use std::ops::{Add, Sub, Mul, Div};
+use std::{fmt::{Display, Debug}, ops::{Add, AddAssign, Div, DivAssign, Mul, MulAssign, Sub, SubAssign}};
 
-use rand::{Rng, prelude::Distribution};
+use rand::{thread_rng, Rng};
 
-trait NodeAble<T>: Add + Sub + Mul + Div + Clone + Copy {}
+/// This trait is used for specifying what can be used as the base type in a node.
+pub trait NodeAble<T>:
+Add<Output = T> + AddAssign +
+Sub<Output = T> + SubAssign +
+Mul<Output = T> + MulAssign +
+Div<Output = T> + DivAssign +
+Sized + Sync +
+Clone + Copy +
+Debug + Display {
+    /// Randomly generates a number between 0 and 1
+    fn get_random() -> Self;
+    /// Gets a value from an f64
+    fn from_f64(n: f64) -> Self;
+    /// Gets a value from a usize
+    fn from_usize(n: usize) -> Self;
+    /// Gets a zeroed value
+    fn new_zero() -> Self;
+}
 
 impl NodeAble<f64> for f64 {
+    fn get_random() -> Self {
+        let mut rng = thread_rng();
+        return rng.gen_range(0.0..1.0);
+    }
 
+    fn from_f64(n: f64) -> Self { return n as Self; }
+    fn new_zero() -> Self { return 0.0; }
+    fn from_usize(n: usize) -> Self { return n as Self; }
+
+}
+
+impl NodeAble<f32> for f32 {
+    fn get_random() -> Self {
+        let mut rng = thread_rng();
+        return rng.gen_range(0.0..1.0);
+    }
+
+    fn from_f64(n: f64) -> Self { return n as Self; }
+    fn new_zero() -> Self { return 0.0; }
+    fn from_usize(n: usize) -> Self { return n as Self; }
 }
 
 /// A struct that represents a node in the nn
@@ -41,10 +77,8 @@ impl<T: NodeAble<T>> Node<T> {
     /// Method for creating a new `Node` except with randomized initial values set between 0 & 1
     pub fn new_random(capacity: usize) -> Self {
 
-        let mut rng = rand::thread_rng();
-
-        let weights: Vec<T> = (0..capacity).map(|_| rng.gen()).collect();
-        let biases: Vec<T> = (0..capacity).map(|_| rng.gen()).collect();
+        let weights: Vec<T> = (0..capacity).map(|_| T::get_random()).collect();
+        let biases: Vec<T> = (0..capacity).map(|_| T::get_random()).collect();
 
         return Self {
             weights,
@@ -53,27 +87,25 @@ impl<T: NodeAble<T>> Node<T> {
 
     }
 
-    pub fn evalute(&self, input: &Vec<T>) -> f64 {
+    /// Method for evaluating a node from the input vec `input`
+    pub fn evalute(&self, input: &Vec<T>) -> T {
 
-        let mut output: T = 0.into();
+        let mut output = T::new_zero();
         let output_count = input.len();
 
         assert_eq!(output_count, input.len());
 
         for i in 0..output_count {
-
             output += self.weights[i] * input[i] + self.biases[i];
-
         }
 
-        return output / (output_count as f64);
+        return output / T::from_usize(output_count);
 
     }
 
 }
 
 /// A struct that represents a layer of nodes
-/// All layers of nodes are inputs to the following node layer
 #[derive(Debug, Clone)]
 pub struct NodeLayer<T> where T: NodeAble<T> {
     /// All the nodes on the layer
@@ -84,11 +116,9 @@ impl<T: NodeAble<T>> NodeLayer<T> {
 
     /// Method for creating a new `NodeLayer` from an array of nodes
     pub fn new_from_nodes(nodes: Vec<Node<T>>) -> Self {
-
         return Self {
             nodes,
         };
-
     }
 
     /// Method for creating a new `NodeLayer`.
@@ -124,58 +154,74 @@ impl<T: NodeAble<T>> NodeLayer<T> {
 
 }
 
-#[derive(Debug)]
-pub struct InputLayer<T> {
-    /// All the nodes on the layer
-    pub nodes: Vec<T>,
-}
+/// A type representing a layer of input nodes
+pub type InputLayer<T> where T: NodeAble<T> = Vec<T>;
+/// A type representing a layer of output nodes
+pub type OutputLayer<T> where T: NodeAble<T> = Vec<T>;
 
-impl<T> InputLayer<T> {
-
-    /// Creates a new `InputLayer`
-    pub fn new(nodes: Vec<T>) -> Self {
-
-        return Self {
-            nodes,
-        };
-
-    }
-
-}
-
-impl InputLayer<f64> {
-
-}
-
+/// A struct representing an entire network.
+/// This struct holds both an input layer and multiple evaluation layers.
 pub struct LayerList<T>
 where T: NodeAble<T> {
-    pub layers: Vec<NodeLayer<T>>,
+    /// This is the input layer that gets fed into the network.
     pub input_layer: InputLayer<T>,
+    /// These are the inner layers or "hidden layers" that make up the network.
+    pub layers: Vec<NodeLayer<T>>,
 }
 
 
 impl<T: NodeAble<T>> LayerList<T> {
 
-    /*
-    pub fn evaluate(&self) -> Vec<T> {
+    /// Method for creating new `LayerList`.
+    pub fn new(input_layer: InputLayer<T>, layers: Vec<NodeLayer<T>>) -> Self {
+        return Self {
+            input_layer,
+            layers,
+        };
+    }
 
-        let tmp_values: Vec<&T> = self.input_layer.nodes.iter()
-            .map(|v| v)
-            .collect();
+    /// Initializes `LayerList` with random values for all its nodes.
+    pub fn new_random(input_layer: InputLayer<T>, amnt_of_layers: usize, node_count: usize) -> Self {
 
-        loop {
+        assert_ne!(amnt_of_layers, 0); // ensures we have at least 1 layer
 
-            tmp_values = next_layer.evaluate(&self.nodes);
-            next_layer = match &next_layer.next_node_layer {
-                Some(v) => v.clone(),
-                None => break,
-            };
+        // We initialize ourself first
+        let mut this = Self {
+            input_layer,
+            layers: Vec::with_capacity(amnt_of_layers),
+        };
 
+        // We then add the first layer outself while specifying out amount of nodes in the input
+        this.push(NodeLayer::new_random((&this.input_layer).len(), node_count));
+
+        // And finally we can use a for loop to initialize all other values
+        for _i in 1..amnt_of_layers {
+            this.push(NodeLayer::new_random(node_count, node_count));
+        }
+
+        // Then we return the created `LayerList`
+        return this;
+
+    }
+
+    /// Method for adding a NodeLayer to the layers
+    pub fn push(&mut self, value: NodeLayer<T>) -> () {
+        return self.layers.push(value);
+    }
+
+    /// Evaluates the neural network based on its attributes.
+    pub fn evaluate(&self) -> OutputLayer<T> {
+
+        // We actually do want to clone here.
+        // The tmp_values are going to be set over and over so having an owned vec is ideal.
+        let mut tmp_values = self.input_layer.clone();
+
+        for i in &self.layers {
+            tmp_values = i.evaluate(&tmp_values);
         }
 
         return tmp_values;
 
     }
-    */
 
 }
--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,17 @@
-mod lib;
-
-use crate::lib::node::{NodeLayer, InputLayer};
+use ai_in_rust::node::LayerList;
 
 fn main() {
 
     let amnt_of_nodes_per_layer = 6;
     let amnt_of_layers = 10;
 
-    let input_nodes = vec![1.0, 2.0, 3.0, 4.0, 5.0];
+    let layer_list = LayerList::new_random(
+        vec![1.0, 2.0, 3.0, 4.0, 5.0],
+        amnt_of_layers,
+        amnt_of_nodes_per_layer,
+    );
 
-    // Initializes `layers`
-    let mut layers: Vec<NodeLayer<f64>> = Vec::with_capacity(amnt_of_layers);
-
-    layers.push(NodeLayer::new_random(amnt_of_nodes_per_layer, amnt_of_nodes_per_layer, None));
-
-    for i in (amnt_of_layers-1)..=1 {
-
-        let prev_count = match i == 1 {
-            true => input_nodes.len(),
-            false => amnt_of_nodes_per_layer,
-        };
-
-        layers.push(NodeLayer::new_random(prev_count, amnt_of_nodes_per_layer, Some(&layers[i-1])));
-
-    }
-
-    // Makes sure there is at least 1 value in the array
-    assert_ne!(layers.len(), 0);
-
-    let mut input = InputLayer::new(input_nodes, Some(&layers[0]));
-
-    // Sets the input nodes "next" as the first value
-    input.next_node_layer = Some(&layers[0]);
-
-    let response = input.evaluate();
+    let response = layer_list.evaluate();
 
     println!("Outputs: {:?}", response);
 
